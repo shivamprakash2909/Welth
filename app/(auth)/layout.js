654e4adc0cd673e7f00be2d4fc2576dbb462cda0
Shivam Prakash <shivamprakash444@gmail.com>
2025-05-23 20:43:22 +0530

    Header & Footer added

---
+++ app/(auth)/layout.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const authLayout = ({ children }) => {
+  return <div className="flex justify-center p-40">{children}</div>;
+};
+
+export default authLayout;
---
+++ app/page.js
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="mt-40">
+      <Button variant="destructive"> destructive button</Button>
+    </div>
+  );
+}
